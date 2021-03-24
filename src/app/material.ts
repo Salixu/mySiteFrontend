bcebd5d4447f104845b68e407674fc7c34b9abdc
bartossh <bartoszglanowski@gmail.com>
2021-03-24 23:30:35 +0100

    Added material icons and started about me section

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
+import { MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -8,10 +9,12 @@ import {MatDividerModule} from '@angular/material/divider';
   imports: [
     MatButtonModule,
     MatDividerModule,
+    MatIconModule
   ],
   exports: [
     MatButtonModule,
     MatDividerModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
