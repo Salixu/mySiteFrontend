ad8f4b3d18028dfe03bf1e922570caa7a4ad5a17
bartossh <bartoszglanowski@gmail.com>
2021-03-25 19:44:38 +0100

    Added animation of my picture(pretty funny).

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {MatIconRegistry} from '@angular/material/icon';
-import {DomSanitizer} from '@angular/platform-browser';
-
-const ARROW_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><g><rect fill="none" height="24" width="24"/><path d="M12,4c4.41,0,8,3.59,8,8s-3.59,8-8,8s-8-3.59-8-8S7.59,4,12,4 M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10 c5.52,0,10-4.48,10-10C22,6.48,17.52,2,12,2L12,2z M13,12l0-4h-2l0,4H8l4,4l4-4H13z"/></g></svg>
-`;
 
 
 
@@ -16,10 +10,8 @@ const ARROW_ICON = `
 export class AboutMeComponent implements OnInit {
 
   constructor(
-    iconRegistry: MatIconRegistry,
-    private sanitizer: DomSanitizer,
+
   ) {
-    iconRegistry.addSvgIconLiteral('arrow_icon', sanitizer.bypassSecurityTrustHtml(ARROW_ICON));
   }
 
   // TODO SMOOTH PAGE TRANSITION
